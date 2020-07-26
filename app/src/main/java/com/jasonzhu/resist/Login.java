98190281654048ac2ae43dc664f34bcc05d2e0ae
jasonmeme <55606978+jasonmeme@users.noreply.github.com>
2020-07-25 21:53:43 -0400

    added homepage and profile

--- app/src/main/java/com/jasonzhu/resist/Login.java
+++ app/src/main/java/com/jasonzhu/resist/Login.java
@@ -2,7 +2,9 @@ package com.jasonzhu.resist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goBack(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void logIn(View view){
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
